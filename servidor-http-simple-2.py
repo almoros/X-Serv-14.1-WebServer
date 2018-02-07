dc2c1bc974d0855b04c2a59756262b24a6655e18
almoros <javieraf88@gmail.com>
2018-02-07 13:17:28 +0100

    Versión 1 del web server

--- servidor-http-simple-2.py
+++ servidor-http-simple-2.py
@@ -22,7 +22,7 @@ mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 mySocket.bind((socket.gethostname(), 1235))
 
 # Queue a maximum of 5 TCP connection requests
-mySocket.listen(5)
+mySocket.listen(1)
 
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
